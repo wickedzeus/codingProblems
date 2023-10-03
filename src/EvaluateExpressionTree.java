7444af616b287d3348a11c2e38cba5b88f8eec6c
Josh Green <green.joshua@gmail.com>
2023-10-02 23:06:54 -0500

    Updated

--- src/EvaluateExpressionTree.java
+++ src/EvaluateExpressionTree.java
@@ -13,8 +13,6 @@ public class EvaluateExpressionTree {
         int left = 0;
         int right = 0;
 
-        if(tree.left.value < 0) left = traverse(tree.left);
-        if(tree.right.value < 0) right = traverse(tree.right);
         if(tree.left.value > 0 && tree.right.value > 0) {
             switch(tree.value) {
                 case -1: return tree.left.value + tree.right.value;
@@ -24,6 +22,17 @@ public class EvaluateExpressionTree {
             }
         }
 
+        if(tree.left.value < 0) {
+            left = traverse(tree.left);
+        } else {
+            left = tree.left.value;
+        }
+        if(tree.right.value < 0) {
+            right = traverse(tree.right);
+        } else {
+            right = tree.right.value;
+        }
+
         return switch (tree.value) {
             case -1 -> left + right;
             case -2 -> left - right;
@@ -31,11 +40,10 @@ public class EvaluateExpressionTree {
             case -4 -> left * right;
             default -> left;
         };
-
     }
 
     public int evaluateExpressionTree(BinaryTree tree) {
 
-        return -1;
+        return traverse(tree);
     }
 }
---
+++ src/MinimumWaitingTime.java
@@ -0,0 +1,6 @@
+public class MinimumWaitingTime {
+    public int minimumWaitingTime(int[] queries) {
+
+        return -1;
+    }
+}
